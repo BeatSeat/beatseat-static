927f11bf801cdec275b02f451b03966660b676a8
BeatSeat <137616995+BeatSeat@users.noreply.github.com>
2025-03-13 01:42:46 +0800

    Update npm-version-bump.js

--- npm-version-bump.js
+++ npm-version-bump.js
@@ -1,26 +1,26 @@
 const fs = require('fs/promises')
 
 const pageageFile = {
-  "name": "volantis-static",
+  "name": "beatseat-static",
   "version": `0.0.${Date.now()}`,
-  "description": "This is volantis static",
+  "description": "This is beatseat's static for blogs",
   "main": "README.md",
   "scripts": {
     "test": "echo \"Error: no test specified\" && exit 1"
   },
   "repository": {
     "type": "git",
-    "url": "git+https://github.com/volantis-x/volantis-static.git"
+    "url": "git+https://github.com/BeatSeat/beatseat-static.git"
   },
   "dependencies": {
   },
-  "keywords": ["volantis", "static"],
-  "author": "Volantis Team",
+  "keywords": ["beatseat", "static"],
+  "author": "BeatSeat",
   "license": "The Unlicense",
   "bugs": {
-    "url": "https://github.com/volantis-x/volantis-static/issues"
+    "url": "https://github.com/BeatSeat/beatseat-static/issues"
   },
-  "homepage": "https://github.com/volantis-x/volantis-static#readme"
+  "homepage": "https://github.com/BeatSeat/beatseat-static#readme"
 }
 const pageageFileStr = JSON.stringify(pageageFile)
 fs.writeFile('./package.json', pageageFileStr, 'utf8', (err) => {
